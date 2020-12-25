cdfedba845859828f6262f8b69e5f447765ebaf4
kukabgd <kukurekovic.stefan@gmail.com>
2020-12-25 21:29:30 +0100

    FirstChapter

---
+++ SWA/SWA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fuck yall, Im going home, to get me some chicken.");
+        }
+    }
+}
