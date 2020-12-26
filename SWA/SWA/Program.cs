24c9984bb99f483cd5667fe96b98b0eacc40c7fd
kukabgd <kukurekovic.stefan@gmail.com>
2020-12-26 01:18:28 +0100

    v2

--- SWA/SWA/Program.cs
+++ SWA/SWA/Program.cs
@@ -6,7 +6,20 @@ namespace SWA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fuck yall, Im going home, to get me some chicken.");
+            string [] numbers = new string[5];
+            
+            Console.Write("Please enter 5 names: ");
+            for(int i = 0; i < 5; i++)
+            {
+                numbers[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("You've entered following names:");
+            foreach(string i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
