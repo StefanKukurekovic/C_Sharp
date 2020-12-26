b0d33a1d9a074d17fb5a64c2966dd6d59a949ab4
kukabgd <kukurekovic.stefan@gmail.com>
2020-12-26 14:11:29 +0100

    v3

--- SWA/SWA/Class1.cs
+++ SWA/SWA/Class1.cs
@@ -7,27 +7,80 @@ namespace SWA
     public class Student
     {
         private string name;
-        /*private int age;
-        private int marksInMaths;
-        private int marksInEnglish;
-        private int marksInScience;
+        private int age;
+        private int marksOfMaths;
+        private int marksOfEnglish;
+        private int marksOfScience;
         private int totalMarks = 300;
         private int obtainedMarks;
-        private double precentage;*/
+        private double precentage;
 
         // Name
         public string Name 
         { 
-            get
-            {
-                return name;
-            }
+            get { return name; }
+            set { name = value; }
+        }
+
+        //Age
+        public int Age 
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        //Math
+        public int MarksOfMaths
+        {
+            get { return marksOfMaths; }
+            set { IfCondition(marksOfMaths, value); }
+        }
+
+        //English
+        public int MarksOfEnglish
+        {
+            get { return marksOfEnglish; }
+            set { IfCondition(marksOfEnglish, value); }
+        }
+
+        //Science
+        public int MarksOfScience
+        {
+            get { return marksOfScience; }
+            set { IfCondition(marksOfScience, value); }
+        }
+
+        // Obtained Marks
+        public int CalculateTotalMarks 
+        {
+            get { return obtainedMarks; }
             set
             {
-                name = value;
+                obtainedMarks = marksOfMaths + marksOfEnglish + marksOfScience;
             }
         }
 
-       
+        public double CalculatePercentage 
+        { 
+            get { return precentage; }
+            set
+            {
+                precentage = (double)obtainedMarks / totalMarks * 100;
+            }
+        }
+
+        public void IfCondition(int var, int value)
+        {
+            if (value >= 0 && value <= 100)
+            {
+                var = value;
+            }
+            else
+            {
+                var = 0;
+            }
+        }
+
+
     }
 }
--- SWA/SWA/Program.cs
+++ SWA/SWA/Program.cs
@@ -7,15 +7,49 @@ namespace SWA
         static void Main(string[] args)
         {
             Student theStudent = new Student();
-            Student[] students = new Student[2];
 
-            theStudent.Name = "Stefan";
-            String myName = theStudent.Name;
-            theStudent.Name = "Faraz";
+            Console.Write("How many students would you like to enter: ");
+            string numStudents = Console.ReadLine();
+            int num = int.Parse(numStudents);
+
+            Student[] students = new Student[num];
+
+            int count = 0;
+            do
+            {
+                Console.Write("Please enter student's first name: ");
+                theStudent.Name = Console.ReadLine();
+
+                Console.Write("Please enter student's age: ");
+                theStudent.Age = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please enter student's English marks: ");
+                theStudent.MarksOfEnglish = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please enter student's Science marks: ");
+                theStudent.MarksOfScience = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please enter student's Math marks: ");
+                theStudent.MarksOfMaths = Convert.ToInt32(Console.ReadLine());
+
+                students[count] = theStudent;
+
+
+                count++;
+            } while (count != num);
+
+                        
+            foreach(Student s in students)
+            {
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine(s.Name);
+                Console.WriteLine(s.Age);
+            }
 
             //theStudent.name = "Faraz";
-            Console.WriteLine(myName);
-            Console.WriteLine(theStudent.name);
+            //Console.WriteLine(myName);
+            //Console.WriteLine(theStudent.name);
 
             /*st1.SetName = "Einstein";
             st1.SetAge = 20;
