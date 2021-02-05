0cd2538a7f94e184049e5161b164643f7eadc2b3
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-05 20:25:42 +0100

    v2.0

--- UdmBook/UdmBook/Program.cs
+++ UdmBook/UdmBook/Program.cs
@@ -6,9 +6,35 @@ namespace UdmBook
     {
         static void Main(string[] args)
         {
-            Members memb = new Members();
-            memb.Introducing(false);
-            Console.ReadKey();
+          
+            int[,] array2D = new int[,]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+            };
+
+            Console.WriteLine("Center value is {0}", array2D[2,0]);
+
+            string[,,] array3D = new string[,,]
+            {
+                {
+                    {"000", "001"},
+                    {"010", "011"}
+                },
+                {
+                    {"100", "101"},
+                    {"110", "111"}
+                }
+            };
+
+            Console.WriteLine("Center value is {0}", array3D[1, 1, 0]);
+
+            int dimensions = array3D.Rank;
+            Console.WriteLine(dimensions);
+
+
+
         }
     }
 }
