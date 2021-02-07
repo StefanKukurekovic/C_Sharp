9cbf61a607c776d200b25ff8e2ea092445485cb1
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-07 13:12:00 +0100

    v3.1

--- UdmBook/InherUdm/InherUdm/Program.cs
+++ UdmBook/InherUdm/InherUdm/Program.cs
@@ -6,17 +6,13 @@ namespace InherUdm
     {
         static void Main(string[] args)
         {
-            Post post1 = new Post("Thank you for birthday wishes, bitches!", true, "xoxo Gossip Girl");
-            Post post2 = new Post("Oh btw, tonight party at my place!", false, "xoxo GG");
-            post1.PrintConsole();
-            post2.PrintConsole();
-            Console.WriteLine("--------------------------------------------------------");
-            Console.WriteLine(post1.ToString());
-            Console.WriteLine(post2.ToString());
-
-            ImagePost imgOne = new ImagePost("Check out my new shoes", "Stefan Kuurekovic", "https://images.com/shoes", true);
-
-            Console.WriteLine(imgOne.ToString());
+            VideoPost videoPost = new VideoPost("Hey, check my new YouTube video. Hope you like it.", "Stefan Kukurekovic", true, "http://youtube.com/kukabgd/new_year_resolutions", 5);
+            videoPost.Play();
+            Console.ReadKey();
+            videoPost.Stop();
+           
         }
+
+        
     }
 }
---
+++ UdmBook/InherUdm/InherUdm/VideoPost.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace InherUdm
+{
+    class VideoPost : Post
+    {
+        protected bool isPlaying = false;
+        protected int currDuration = 0;
+        Timer timer;
+        public string VideoURL { get; set; }
+        public int Length { get; set; }
+
+        public VideoPost() { }
+        public VideoPost(string title, string sendByUsername, bool isPublic, string videoUrl, int length)
+        {
+            this.id = GetNextId();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = IsPublic;
+
+            this.VideoURL = videoUrl;
+            this.Length = length;
+        }
+
+        public void Play()
+        {
+            if (!isPlaying)
+            {
+                Console.WriteLine("Playing...");
+                timer = new Timer(TimerCallback, null, 0, 1000);
+            }
+        }
+
+        private void TimerCallback(Object o)
+        {
+            if(currDuration < Length)
+            {
+                currDuration++;
+                Console.WriteLine("Video at {0}s", currDuration);
+                GC.Collect();
+            }
+            else
+            {
+                Stop();
+            }
+
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped at {0}", currDuration);
+            currDuration = 0;
+            timer.Dispose();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - length: {3} min - by {4}", this.id, this.Title, this.VideoURL, this.Length, this.SendByUsername);
+        }
+      
+
+    }
+}
