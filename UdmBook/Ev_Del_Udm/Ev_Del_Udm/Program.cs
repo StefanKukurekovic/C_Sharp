50ad34bc4288f28961cf654c5922b6d22fb78b77
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-11 23:15:27 +0100

    v5.2

---
+++ UdmBook/Ev_Del_Udm/Ev_Del_Udm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ev_Del_Udm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
