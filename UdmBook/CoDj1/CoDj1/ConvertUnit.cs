c0e48a7e6922a297235a09c82fd100250f5014d2
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-12 21:30:33 +0100

    v6

---
+++ UdmBook/CoDj1/CoDj1/ConvertUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoDj1
+{
+    class ConvertUnit
+    {
+        public void UserInput(int userInput)
+        {
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("Please enter a value of a Celsius: ");
+                    CelsiusToFahrenheit(Int32.Parse(Console.ReadLine()));
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter a value of a Celsius: ");
+                    CelsiusToKelvin(Int32.Parse(Console.ReadLine()));
+                    break;
+            }
+        }
+        private void CelsiusToKelvin(double c)
+        {
+            Console.WriteLine("{0} Celsius is {1} Kelvin.", c, (c + 274.15));
+        }
+
+        private void CelsiusToFahrenheit(double c)
+        {
+            Console.WriteLine("{0} Celsius is {1} Fahrenheit.", c, (c*9/5)+32);
+        }
+    }
+}
---
+++ UdmBook/CoDj1/CoDj1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CoDj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            bool oneOrTwo = false;
+
+            ConvertUnit convertUnit = new ConvertUnit();
+            // NON OO: Ask user after input take [Fahrenheit, Kelvin]
+            do
+            {
+                Console.WriteLine("Please choose the unit you want to convert Celsius to:");
+                Console.WriteLine("\t1) Fahrenheit \n\t2) Kelvin");
+
+                try
+                {
+                    userInput = Int32.Parse(Console.ReadLine());
+                }catch(Exception e)
+                {
+                    Console.WriteLine("[Exception Message]: Only nunmbers are allowed.");
+                }
+                
+                if (userInput == 1 || userInput == 2)
+                    oneOrTwo = true;
+                else
+                    Console.WriteLine("Please enter only 1 or 2. Try again.");
+
+            } while (!oneOrTwo);
+
+            convertUnit.UserInput(userInput);
+            
+
+
+
+        }
+    }
+}
