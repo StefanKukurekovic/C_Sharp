d2e3125f5ec18dcf8810fa81af9d4b03cc24793c
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-14 00:44:34 +0100

    v7.1

---
+++ SchOolCsHaRp/SchOolCsHaRp/ClockTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SchOolCsHaRp
+{
+    public delegate void TimerEvent(object sender, EventArgs e);
+    class ClockTimer
+    {
+        public event TimerEvent Timer;
+
+        public void Start()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Timer(this, null);
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- SchOolCsHaRp/SchOolCsHaRp/Program.cs
+++ SchOolCsHaRp/SchOolCsHaRp/Program.cs
@@ -4,66 +4,17 @@ namespace SchOolCsHaRp
 {
     class Program
     {
-        delegate int MyDelegate(int p, int q);
-        delegate void MyMulticastDelegate(int p, int q);
         static void Main(string[] args)
         {
-            //MyDelegate arithMethod = new MyDelegate(Add);
-            MyMulticastDelegate arMethods = null;
-            arMethods = new MyMulticastDelegate(Add);
-            arMethods += new MyMulticastDelegate(Substract);
-            arMethods += new MyMulticastDelegate(Max);
-
-            arMethods(3, 4);
-
-            //PerformArithOperation(3, 4, arithMethod);
-            /*
-            Console.WriteLine("Choose");
-            Console.WriteLine("Press + for Add");
-            Console.WriteLine("Press - for Substract");
-            Console.WriteLine("Press m for Maximum Number");
-            char choice = (char)Console.Read();
-
-            switch(choice)
-            {
-                case '+':
-                    arithMethod = new MyDelegate(Add);
-                    break;
-                case '-':
-                    arithMethod = new MyDelegate(Substract);
-                    break;
-                case 'm':
-                    arithMethod = new MyDelegate(Max);
-                    break;
-            }
-            int r = arithMethod(3, 4);
-            Console.WriteLine("Result is {0}.", r);
-            */
-           
+            ClockTimer clockTimer = new ClockTimer();
+            //clockTimer.Timer += new TimerEvent(onClockTick);
+            clockTimer.Start();
         }
-        static void Add(int a, int b)
+        /*
+        public static void onClockTick(object sender, EventArgs e)
         {
-            Console.WriteLine("The sum of 3 and 4 is {0}.", (a + b));
-        }
-
-        static void Substract(int a, int b)
-        {
-            Console.WriteLine("The substraction of 3 and 4 is {0}.", (a - b));
-        }
-
-        static void Max(int a, int b)
-        {
-            if (a > b)
-                Console.WriteLine("The larger number is {0}.", a);
-            else
-                Console.WriteLine("The larger number is {0}.", b);
-        }
-
-        static void PerformArithOperation(int a, int b, MyDelegate arithOperation)
-        {
-            int r = arithOperation(a, b);
-            Console.WriteLine("The result on arithmetic operation od 3 and 4 is {0}.", r);
-        }
+            Console.WriteLine("Recived a clock tick event...");
+        }*/
 
 
     }
