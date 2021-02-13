81f7a527faa0eea4b6aac1d6770fad448f0b2062
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-13 09:28:09 +0100

    v6.1

---
+++ Zoo2/Zoo2/Cage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo2
+{
+    class Cage
+    {
+    }
+}
---
+++ Zoo2/Zoo2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zoo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
