21fff08e10308ecb320918245c4b60ba93007fe2
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-13 12:59:21 +0100

    v6.5

---
+++ SchOolCsHaRp/SchOolCsHaRp/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchOolCsHaRp
+{
+    struct Point
+    {
+        public double x;
+        public double y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+}
---
+++ SchOolCsHaRp/SchOolCsHaRp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SchOolCsHaRp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pt2;
+            pt2.x = 6;
+            pt2.y = 3;
+
+            Console.WriteLine("pt2 = {0}", pt2);
+
+        }
+    }
+}
