a66492b3641ac90b524c7e9d0b03e10cf524ea43
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-13 11:26:15 +0100

    v6.2

---
+++ Zoo2/Zoo2/Zoo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo2
+{
+    class Zoo
+    {
+        public List<Animal> Animals { get; set; }
+        public string Name { get; private set; }
+        public List<Cage> Cages { get; set; }
+        public Zookeeper Zookeper { get; set; }
+
+        public Zoo(string name)
+        {
+            Name = name;
+        }
+
+        public void AddAnimal(Animal a)
+        {
+            if(Animals == null)
+            {
+                Animals = new List<Animal>();
+            }
+            Animals.Add(a);
+        }
+
+        public void AddCage(Cage ca)
+        {
+            if(Cages == null)
+            {
+                Cages = new List<Cage>();
+            }
+            Cages.Add(ca);
+        }
+    }
+}
