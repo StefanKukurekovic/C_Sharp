d619968c88cded5eb8dd05d4fe80eb3f542987f0
kukabgd <kukurekovic.stefan@gmail.com>
2021-02-21 00:19:23 +0100

    v14

---
+++ WpfApp9/WpfApp9/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+
+namespace WpfApp9.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private bool isClickable = false;
+        private string text = "";
+        private int clickCounter = 0;
+
+        RelayCommand clickBtnCommand;
+        RelayCommand clickBtnommand2;
+
+        public int ClickCounter
+        {
+            get { return clickCounter; }
+            set { clickCounter = value; RaisePropertyChanged("ClickCoounter"); }
+        }
+
+        public bool IsClickable
+        {
+            get { return isClickable; }
+            set { isClickable = value; }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public RelayCommand ClickBtnCommand
+        {
+            get { return clickBtnCommand; }
+            set { clickBtnCommand = value; }
+        }
+
+        public RelayCommand ClickBtnCommand2
+        {
+            get { return clickBtnommand2; }
+            set { clickBtnommand2 = value; }
+        }
+
+        public MainViewModel()
+        {
+            ClickBtnCommand = new RelayCommand(new Action(ExecuteClickCount), new Func<bool>(CanExecuteClickCount));
+            ClickBtnCommand = new RelayCommand(ExecuteClickCount, CanExecuteClickCount);
+            ClickBtnCommand2 = new RelayCommand(ExecuteClickCount, () => { if (Text.Length > 3) { return true; } else { return false; } });
+
+        }
+
+        private bool CanExecuteClickCount()
+        {
+            return IsClickable;
+        }
+
+        private void ExecuteClickCount()
+        {
+            ClickCounter++;
+        }
+    }
+}
